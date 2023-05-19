bd42182e8315185cc07a6256d607b5dbf88e5576
minhbdk <khaminhbui@gmail.com>
2023-05-19 17:34:29 +0700

    init src + section 5

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,20 @@
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages() {
+    //TODO: code here
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+  }
+}
